6ba3ea80746f621e7c56abb0621de6b411577a7c
Son Nguyen <trgson250411@gmail.com>
2023-10-11 23:38:32 +0700

    New Update

--- backend/src/main/java/com/example/demo/entities/Product.java
+++ backend/src/main/java/com/example/demo/entities/Product.java
@@ -1,5 +1,43 @@
 package com.example.demo.entities;
 
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+@Setter
 public class Product {
+    @Id
+    @Column(nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "\"description\"")
+    private String description;
+
+    @Column(nullable = false)
+    private Long size_id;
+
+    @Column(nullable = false) 
+    private Long color_id;
+
+    @Column(nullable = false)
+    private String materials;
+
+    @Column
+    private String image;
+
+    @Column(nullable = false)
+    private String instruction;
     
+    @Column(nullable = false)
+    private Long price;
 }
