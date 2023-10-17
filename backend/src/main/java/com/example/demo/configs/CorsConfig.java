521fb3ab08644c7c2545cc8dc7ef7a685bd63dd6
unknown <buisonvpvttt@gmail.com>
2023-10-17 20:28:27 +0700

    Add CorsConfiguration

---
+++ backend/src/main/java/com/example/demo/configs/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.configs;
+
+import javax.persistence.Entity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Entity 
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "DELETE")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
