b87616ce4fed8203820f86738dbb2b773981f663
unknown <buisonvpvttt@gmail.com>
2023-10-17 20:37:51 +0700

    update

--- backend/src/main/java/com/example/demo/entities/CartItem.java
+++ backend/src/main/java/com/example/demo/entities/CartItem.java
@@ -1,33 +1,33 @@
-package com.example.demo.entities;
-import com.example.demo.entities.User;
-import com.example.demo.entities.Product;
+// package com.example.demo.entities;
+// import com.example.demo.entities.User;
+// import com.example.demo.entities.Product;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.FetchType;
+// import javax.persistence.Column;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.Id;
+// import javax.persistence.JoinColumn;
+// import javax.persistence.ManyToOne;
+// import javax.persistence.FetchType;
 
-import javax.persistence.GenerationType;
+// import javax.persistence.GenerationType;
 
-@Entity
-public class CartItem {
-    @Id
-    @Column(nullable = false, updatable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+// @Entity
+// public class CartItem {
+//     @Id
+//     @Column(nullable = false, updatable = false)
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
 
-    @Column(nullable = false)
-    private String name;
+//     @Column(nullable = false)
+//     private String name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+//     @ManyToOne(fetch = FetchType.LAZY)
+//     @JoinColumn(name = "user_id")
+//     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="product_id")
-    private Product product;
+//     @ManyToOne(fetch = FetchType.LAZY)
+//     @JoinColumn(name="product_id")
+//     private Product product;
 
-}
+// }
--- backend/src/main/java/com/example/demo/services/CartItemService.java
+++ backend/src/main/java/com/example/demo/services/CartItemService.java
@@ -1,15 +1,15 @@
-package com.example.demo.services;
-import com.example.demo.repositories.CartItemRepository;
+// package com.example.demo.services;
+// import com.example.demo.repositories.CartItemRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+// import org.springframework.transaction.annotation.Transactional;
 
-@Service
-public class CartItemService {
-    @Autowired
-    private CartItemRepository cartItemRepository;
+// @Service
+// public class CartItemService {
+//     @Autowired
+//     private CartItemRepository cartItemRepository;
 
 
 
-}
+// }
