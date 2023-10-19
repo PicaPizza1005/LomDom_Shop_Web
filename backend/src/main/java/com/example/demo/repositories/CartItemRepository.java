e3137dbf067c5bc0a2fbb5392bf67a6304867745
unknown <buisonvpvttt@gmail.com>
2023-10-19 16:26:22 +0700

    update

--- backend/src/main/java/com/example/demo/repositories/CartItemRepository.java
+++ backend/src/main/java/com/example/demo/repositories/CartItemRepository.java
@@ -1,8 +1,8 @@
-package com.example.demo.repositories;
-import com.example.demo.entities.CartItem;
+// package com.example.demo.repositories;
+// import com.example.demo.entities.CartItem;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+// public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     
-}
+// }
