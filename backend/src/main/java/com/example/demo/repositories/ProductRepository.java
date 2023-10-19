8507cfdd265b9a9322edb7e555d28a14c5452a5b
sweetbacon <nguyentrungsb1005@gmail.com>
2023-10-19 07:22:14 +0700

    'update'

--- backend/src/main/java/com/example/demo/repositories/ProductRepository.java
+++ backend/src/main/java/com/example/demo/repositories/ProductRepository.java
@@ -1,8 +1,10 @@
-// package com.example.demo.repositories;
-// import com.example.demo.entities.Product;
+package com.example.demo.repositories;
+import com.example.demo.entities.Product;
 
-// import java.util.List;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// public interface ProductRepository extends JpaRepository<Product, Long>{
-//     List<Product> findAllByCategoryId(Long categoryId);
-// }
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ProductRepository extends JpaRepository<Product, Long>{
+    List<Product> findAllByCategoryId(Long id);
+    List<Product> findAllByCategoryIdAndSizeId(Long id, Long size_id);
+    List<Product> findAllByCategoryIdAndColorId(Long id, Long color_id);
+}
--- backend/src/main/java/com/example/demo/services/ProductService.java
+++ backend/src/main/java/com/example/demo/services/ProductService.java
@@ -1,11 +1,1 @@
-// package com.example.demo.services;
-// import com.example.demo.repositories.ProductRepository;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-
-// @Service
-// public class ProductService {
-//     @Autowired
-//     private ProductRepository productRepository;
-// }
