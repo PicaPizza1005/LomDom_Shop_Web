4f8e282ee61f587c9da887f3b56696d6fb3983f6
sweetbacon <nguyentrungsb1005@gmail.com>
2023-11-26 00:09:34 +0700

    update

--- frontend/js/shop.js
+++ frontend/js/shop.js
@@ -1,65 +1,50 @@
 const api = "http://localhost:8081";
-// const product = document.getproducts[i]ById("product");
-// async function getProduct() {
-//     try {
-//         const response = await fetch(`${api}/api/v1/products`);
-//         const products = await response.json();
-//         console.log(products);
-//         product.innerHTML="";
-//         products.forEach((products[i]) => {
-//             product.innerHTML += `
-//             <div class="col-lg-4 col-md-6 col-sm-6">
-//                 <div class="product__item">
-//                     <div products-setbg="${products[i].image}" class="product__item__pic set-bg" products width="260" height="260">
-//                         <ul class="product__hover">
-//                             <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
-//                         </ul>
-//                     </div>
-//                     <div class="product__item__text">
-//                         <h6>${products[i].name}</h6>
-//                         <a href="#" class="add-cart">+ Add To Cart</a>
-//                         <h5>${products[i].price}<i class="fa fa-money"></i></h5>
-                        
-//                     </div>
-//                 </div>
-//             </div>
-            
-//             `;
-//         });
-//     }
-//     catch (error) {
-//         console.error("Error fetching product products:", error);
-//     }
-//     $('.set-bg').each(function() {
-//         var bg = $(this).products('setbg');
-//         $(this).css('background-image', 'url(' + bg + ')');
-//     });
-// }
-// getProduct();
-const product__pagination = document.getElementById("product__pagination");
 const product_div = document.getElementById("product");
+const product__pagination = document.getElementById("product__pagination");
 
-let pageNo = 0;
-let pageSize = 9;
-let startIndex = 0;
-let endIndex = 9;
+let pageNo = 1;
+let pageSize = 18;
+let totalPages = 0;
+let categories = [];
+
+getProduct().then(() => {
+    loadProduct(products);
+});
 
 async function getProduct() {
     try {
         const response = await fetch(`${api}/api/v1/products`);
-        const products = await response.json();
-        loadProduct(products);
+        products = await response.json();
     }
     catch (error) {
-        console.error("Error fetching product products0:", error);
+        console.error("Error fetching product:", error);
     }
 }
-getProduct();
+
+async function getCategory() {
+    try {
+        const response = await fetch(`${api}/api/v1/categories`);
+        categories = await response.json();
+    }
+    catch (error) {
+        console.error("Error fetching category:", error);
+    }
+}
+
+async function loadProductByCategory(category) {
+    try {
+        const response = await fetch(`${api}/api/v1/products/category/${category}`);
+        productByCategory = await response.json();
+        loadProduct(productByCategory);
+    }
+    catch (error) {
+        console.error("Error fetching product by category:", error);
+    }
+}
 
 function loadProduct(products) {
     try {
-        console.log(products);
-        for (let i = pageNo*pageSize; i<(pageNo*pageSize)+pageSize; i++) {
+        for (let i = pageNo*pageSize-pageSize; i<pageNo*pageSize; i++) {
             if (!products[i]) 
                 break; 
             product_div.innerHTML += `
@@ -80,25 +65,39 @@ function loadProduct(products) {
             
             `;
         }
-        $('.set-bg').each(function() {
-            var bg = $(this).products('setbg');
-            $(this).css('background-image', 'url(' + bg + ')');
-        });
-        // loadPagination(products);    
+        loadPageList(products);
     }
     catch (error) {
-        console.error("Error fetching product products:", error);
+        console.error("Error rendering product products:", error);
     }
+    $('.set-bg').each(function() {
+        var bg = $(this).attr('products-setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
 }
-function loadPagination(products) {
+function changePage(page) {
+    pageNo = page;
+    product_div.innerHTML = "";
+    loadProduct(products);
+}
+function loadPageList(products) {
     product__pagination.innerHTML = "";
-    for (let i=0; i<(products.length/pageSize); i++) {
-        const span = document.createElement("div");
-        span.innerHTML = i+1;
-        span.addEventListener('click', (e) => {
-            pageIndex = e.target.innerHTML-1;
-            loadItems();
-        });
-        product__pagination.append(span);
+    console.log(products);
+    totalPages = products.length/pageSize;
+    for (let i=1; i<=totalPages+1; i++) {
+        if (i==pageNo) {
+            product__pagination.innerHTML += `
+            <a href="#" onclick="changePage(${i})" class="active">${i}</a>
+            `;
+            continue;
+        }
+        product__pagination.innerHTML += `
+        <a href="#" onclick="changePage(${i})">${i}</a>
+        `;
     }
+    $('.product__pagination a').on('click', function(e) {
+        e.preventDefault();
+        $('.product__pagination a').removeClass('active');
+        $(this).addClass('active');
+    });
 }
